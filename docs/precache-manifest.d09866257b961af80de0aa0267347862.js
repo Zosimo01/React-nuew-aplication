83598fde754e99ae42505f13038f459da192e3fa
zosimo torres <zosimo179@gmail.com>
2020-10-31 20:20:21 -0500

    docs agregado

---
+++ docs/precache-manifest.d09866257b961af80de0aa0267347862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce104a98f421fc741f2f1e530e37139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bcb654fcae449bd96d5",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bcb654fcae449bd96d5",
+    "url": "/static/js/main.c60afeaf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
